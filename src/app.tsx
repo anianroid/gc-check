09ea2a371b0ab4b3dcab544bfa95545fd11d29b2
anianroid <anianroid@gmail.com>
2023-11-08 15:09:45 +0530

    a

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.body)
-root.render(<h2>Hello from React adddasd!</h2>)
+root.render(<h2>Hello from React dfgdgdfgd!</h2>)
