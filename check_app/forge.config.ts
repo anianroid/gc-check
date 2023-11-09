0c4d44557c5f874e0742aa395b0127fa4ad69f51
anianroid <anianroid@gmail.com>
2023-11-09 20:22:42 +0530

    s3 setup for publishing

--- check_app/forge.config.ts
+++ check_app/forge.config.ts
@@ -1,26 +1,52 @@
-import { MakerDeb } from '@electron-forge/maker-deb'
-import { MakerRpm } from '@electron-forge/maker-rpm'
-import { MakerSquirrel } from '@electron-forge/maker-squirrel'
-import { MakerZIP } from '@electron-forge/maker-zip'
-import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives'
-import { WebpackPlugin } from '@electron-forge/plugin-webpack'
-import type { ForgeConfig } from '@electron-forge/shared-types'
-import { MakerDMG } from '@electron-forge/maker-dmg'
+import { MakerDeb } from "@electron-forge/maker-deb";
+import { MakerRpm } from "@electron-forge/maker-rpm";
+import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+import { MakerZIP } from "@electron-forge/maker-zip";
+import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
+import { WebpackPlugin } from "@electron-forge/plugin-webpack";
+import type { ForgeConfig } from "@electron-forge/shared-types";
+import { MakerDMG } from "@electron-forge/maker-dmg";
 
-import { mainConfig } from './webpack.main.config'
-import { rendererConfig } from './webpack.renderer.config'
+import { mainConfig } from "./webpack.main.config";
+import { rendererConfig } from "./webpack.renderer.config";
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
   },
   rebuildConfig: {},
+  publishers: [
+    {
+      name: "@electron-forge/publisher-s3",
+      config: {
+        bucket: "my-bucket",
+        public: true,
+      },
+    },
+  ],
   makers: [
     new MakerSquirrel({}),
-    new MakerZIP({}, ['darwin']),
+    new MakerZIP({}, ["darwin"]),
     new MakerRpm({}),
     new MakerDeb({}),
-    new MakerDMG({})
+    new MakerDMG({}),
+    {
+      name: "@electron-forge/maker-zip",
+      config: (arch) => ({
+        // Note that we must provide this S3 URL here
+        // in order to support smooth version transitions
+        // especially when using a CDN to front your updates
+        macUpdateManifestBaseUrl: `https://ani-electron-auto-updater.s3.ap-south-1.amazonaws.com/gc-test/darwin/${arch}/RELEASES.json`,
+      }),
+    },
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: (arch) => ({
+        // Note that we must provide this S3 URL here
+        // in order to generate delta updates
+        remoteReleases: `https://ani-electron-auto-updater.s3.ap-south-1.amazonaws.com/gc-test/win32/${arch}`,
+      }),
+    },
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
@@ -30,17 +56,17 @@ const config: ForgeConfig = {
         config: rendererConfig,
         entryPoints: [
           {
-            html: './src/index.html',
-            js: './src/renderer.ts',
-            name: 'main_window',
+            html: "./src/index.html",
+            js: "./src/renderer.ts",
+            name: "main_window",
             preload: {
-              js: './src/preload.ts',
+              js: "./src/preload.ts",
             },
           },
         ],
       },
     }),
   ],
-}
+};
 
-export default config
+export default config;
