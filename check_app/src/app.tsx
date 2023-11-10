42ac97c3c9841f466d8bdddc4cbd26aab53f1ca4
anianroid <anianroid@gmail.com>
2023-11-10 18:22:57 +0530

    hey ther

--- check_app/src/app.tsx
+++ check_app/src/app.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.body)
-root.render(<h2>'WERGHFDFGDFHDFNHGJN'</h2>)
+root.render(<h2>'HELLLLLLLLLLLLLLLLLLLLLLLLO'</h2>)
